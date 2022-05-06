fcb30c6a223cb7f61a04cc330937c2fb0952ace9
Magdalena Sarandeva <sarandeva2@gmail.com>
2022-05-06 16:00:49 +0200

    Adding  color form & rendered color list

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ import "./style.css";
 import store from "./data";
 import { decrement, increment } from "./data/counter.js";
 import { addTodo } from "./data/todo.js";
+import { voegKleur } from "./data/kleuren.js";
 
 function render() {
   document.querySelector("#app").innerHTML = `<h1>${
     store.getState().counterState.counter
   }</h1>`;
 
-  document.querySelector("#ul").innerHTML = store
+  document.querySelector("ul.todos").innerHTML = store
     .getState()
     .todoState.map((todo) => `<li>${todo.todo}</li>`)
+    .join("");
+
+  document.querySelector("ul.kleuren").innerHTML = store
+    .getState()
+    .kleurState.map((kleur) => `<li>${kleur.hexcode} ${kleur.kleur}</li>`)
     .join("");
 }
 
@@ -26,9 +32,19 @@ document.getElementById("inc").onclick = function () {
   store.dispatch(increment());
 };
 
-const form = document.querySelector("form");
+const form = document.querySelector("form.todos");
 form.onsubmit = function (e) {
   e.preventDefault();
   store.dispatch(addTodo(form.elements["veld"].value));
   form.reset();
 };
+
+//  colors:
+
+const kleurForm = document.querySelector("form.kleuren");
+kleurForm.onsubmit = function (e) {
+  e.preventDefault();
+  store.dispatch(voegKleur(kleurForm.elements["kleur-naam"].value));
+  kleurForm.reset();
+};
+document.querySelector("#voegKleur").onclick = function () {};

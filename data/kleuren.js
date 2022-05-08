4a4c60ae96dc0fd391259cdfa0a06ca5f35faf90
Magdalena Sarandeva <sarandeva2@gmail.com>
2022-05-08 15:39:42 +0200

    Bugg solved

--- data/kleuren.js
+++ data/kleuren.js
@@ -11,22 +11,34 @@ const initialState = [
     kleur: "zuivergroen",
     hexcode: "#008f39",
   },
+  {
+    id: nanoid(4),
+    kleur: "orhidee",
+    hexcode: "#da70d6",
+  },
 ];
 
 const kleurSlice = createSlice({
   name: "kleurState",
   initialState,
   reducers: {
-    voegKleur(state, action) {
+    voegKleur(state = initialState, { payload: { kleur, hexcode } }) {
       state.push({
-        id: nanoid(4), //save it for later (etc.delete/filter)
-        kleur: action.payload.kleur,
-        hexcode: action.payload.hexcode,
+        id: nanoid(4),
+        kleur,
+        hexcode,
       });
+    },
+    veranderKleur(state = initialState, { payload: { hexcode, id } }) {
+      console.log(id);
+      const objectToEdit = state.find((kleur) => kleur.id === id);
+      objectToEdit.hexcode = hexcode;
     },
   },
 });
 
+// console.log(kleurSlice);
+
 // each case under reducers becomes an action
 export default kleurSlice;
-export const { voegKleur } = kleurSlice.actions;
+export const { voegKleur, veranderKleur } = kleurSlice.actions;
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import "./style.css";
 import store from "./data";
 import { decrement, increment } from "./data/counter.js";
 import { addTodo } from "./data/todo.js";
-import { voegKleur } from "./data/kleuren.js";
+import { voegKleur, veranderKleur } from "./data/kleuren.js";
 
 function render() {
   document.querySelector("#app").innerHTML = `<h1>${
@@ -14,18 +14,26 @@ function render() {
     .todoState.map((todo) => `<li>${todo.todo}</li>`)
     .join("");
 
-  document.querySelector("ul.kleuren").innerHTML = store
+  document.querySelector("ul#kleuren").innerHTML = store
     .getState()
-    .kleurState.map(
-      (kleur) =>
-        `<li><div style="background-color: ${kleur.hexcode}"></div> <span style="color:${kleur.hexcode}">${kleur.kleur}</span></li>`
-    )
+    .kleurState.map((kleur) => {
+      // console.log(kleur.id);
+      // console.log(kleur.hexcode);
+      // console.log(kleur.kleur);
+      return `<li>
+      <span id="kpreview" style="background-color: ${kleur.hexcode}">&nbsp;</span>
+      <p style="color: ${kleur.hexcode}" contentEditable>${kleur.kleur} </p>
+      <input value="${kleur.hexcode}" data-id="${kleur.id}" type="color" name="kleur" class="hexcode" />
+      </li>`;
+    })
     .join("");
 }
 
 render();
-
 store.subscribe(render);
+// store.subscribe((render) => {
+//   console.log("Store changed!", store.getState());
+// });
 
 document.getElementById("dec").onclick = function () {
   store.dispatch(decrement());
@@ -42,11 +50,28 @@ form.onsubmit = function (e) {
   form.reset();
 };
 
-//  colors:
-
-const kleurForm = document.querySelector("form.kleuren");
-document.querySelector("#voegKleur").addEventListener("click", function (e) {
-  e.preventDefault();
-  store.dispatch(voegKleur(kleurForm.elements["kleur-veld"].value));
-  kleurForm.reset();
+////////////UI LOGICA VOOR KLEUREN:
+/***
+ * This example demonstrates how to dynamically update a colorpicker value with oninput.
+ */
+document.querySelector("ul#kleuren").addEventListener("change", (e) => {
+  if (e.target.classList.contains("kpicker")) {
+    store.dispatch(
+      veranderKleur({ id: e.target.dataset.id, hexcode: e.target.value })
+    );
+  }
 });
+
+const kform = document.querySelector("#kform");
+kform.onsubmit = function (e) {
+  e.preventDefault();
+  store.dispatch(
+    voegKleur({
+      kleur: kform.elements["kleur"].value,
+      hexcode: kform.elements["hexcode"].value,
+    })
+  );
+  kform.reset();
+};
+
+// console.log(store.kleurState());
